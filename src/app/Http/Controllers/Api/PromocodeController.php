99b9ec8c71a25999c2c1ec5fb991479486c159ae
Андрей <parabellum.koval@gmail.com>
2024-03-26 14:49:19 +0200

    promocode update

--- src/app/Http/Controllers/Api/PromocodeController.php
+++ src/app/Http/Controllers/Api/PromocodeController.php
@@ -19,24 +19,24 @@ class PromocodeController extends \App\Http\Controllers\Controller
   public function findAndCheck(Request $request, $code) {
     
     if(!$code)
-      throw new \Exception(__('promocode.not_found'));
+      return response()->json(['message' => __('promocode.not_found')], 404);
     
     try {  
       $promocode = Promocode::whereRaw('LOWER(`code`) LIKE ? ',[trim(strtolower($code)).'%'])->firstOrFail();
     }catch(\Exception $e) {
-      return response()->json(__('promocode.not_found'), 404);
+      return response()->json(['message' => __('promocode.not_found')], 404);
     }
 
     if($promocode->limit !== 0 && $promocode->used_times >= $promocode->limit) {
-      return response()->json(__('promocode.limit'), 400);
+      return response()->json(['message' => __('promocode.limit')], 400);
     }
 
     if(!$promocode->is_active) {
-      return response()->json(__('promocode.not_active'), 400);
+      return response()->json(['message' => __('promocode.not_active')], 400);
     }
 
     if(Carbon::now()->gt($promocode->valid_until)) {
-      return response()->json(__('promocode.expired'), 400);
+      return response()->json(['message' => __('promocode.expired')], 400);
     }
 
     return $promocode;
