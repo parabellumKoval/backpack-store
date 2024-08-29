6f437c0c7d3706bafebc449d6c4135c098ab498f
Андрей <parabellum.koval@gmail.com>
2024-08-29 17:01:46 +0300

    fix setRules

--- src/app/Models/Source.php
+++ src/app/Models/Source.php
@@ -248,44 +248,4 @@ class Source extends Model
         ]);
       } 
     }
-    
-    /**
-     * setRulesAttribute
-     *
-     * @param  mixed $values
-     * @return void
-     */
-    public function setRulesAttribute($values) {
-      // dd($values);
-      $filterred_value = [];
-
-      foreach($values as $value) {
-        // clear by type
-        if($value->type !== 'overprice') {
-          $value->overprice = null;
-        }
-
-        // clear by target
-        if($value->target !== 'brand') {
-          $value->brands = null;
-        }
-
-        if($value->target !== 'code') {
-          $value->codes = null;
-        }
-
-        if($value->target !== 'name') {
-          $value->names = null;
-        }
-
-        if($value->target !== 'price') {
-          $value->min_price = null;
-          $value->max_price = null;
-        }  
-        
-        $filterred_value[] = $value;
-      }
-
-      $this->attributes['rules'] = json_encode($filterred_value);
-    }
 }
